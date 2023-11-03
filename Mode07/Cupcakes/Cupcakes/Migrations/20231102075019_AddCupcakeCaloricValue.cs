fd5709e437375742ba595b09ad0e243b7f64648f
maria_s_pr@mail.ru <maria_s_pr@mail.ru>
2023-11-03 13:47:28 +0500

    Лабораторная по модулю 7

---
+++ Mode07/Cupcakes/Cupcakes/Migrations/20231102075019_AddCupcakeCaloricValue.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Cupcakes.Migrations
+{
+    public partial class AddCupcakeCaloricValue : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
