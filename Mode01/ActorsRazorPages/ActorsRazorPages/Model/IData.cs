5560f3947ee76b90aa6889911d90eaa98d384aa9
maria_s_pr@mail.ru <maria_s_pr@mail.ru>
2023-11-03 10:37:27 +0500

    Лабораторная по модулю 1

---
+++ Mode01/ActorsRazorPages/ActorsRazorPages/Model/IData.cs
@@ -0,0 +1,15 @@
+﻿using ActorsRazorPages.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ActorsRazorPages.Models
+{
+    public interface IData
+    {
+        List<Actor> ActorsList { get; set; }
+        List<Actor> ActorsInitializeData();
+        Actor GetActorById(int? id);
+    }
+}
